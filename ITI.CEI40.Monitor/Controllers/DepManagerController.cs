aadaf66c03aa4e178d44a419a9b88f04bd91007b
MohamedFarag851 <58907039+MohamedFarag851@users.noreply.github.com>
2020-05-19 05:28:36 +0200

    Dep head and team leader views

---
+++ ITI.CEI40.Monitor/Controllers/DepManagerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITI.CEI40.Monitor.Data;
+using ITI.CEI40.Monitor.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.CEI40.Monitor.Controllers
+{
+    public class DepManagerController : Controller
+    {
+        private readonly IUnitOfWork unitofwork;
+        public DepManagerController(IUnitOfWork unitofwork)
+        {
+            this.unitofwork = unitofwork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TeamsView(int DepId)
+        {
+            IEnumerable<Team> teams= unitofwork.Teams.getTeamsinsideDept(DepId);
+            return View(teams);
+        }
+
+        [HttpGet]
+        public IActionResult displayTasks(int teamId)
+        {
+            var task = unitofwork.Tasks.GetAllTaskWithProject(teamId);
+            return PartialView("_TaskPartialView", task);
+        }
+    }
+}
---
+++ ITI.CEI40.Monitor/Controllers/TeamLeaderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITI.CEI40.Monitor.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.CEI40.Monitor.Controllers
+{
+    public class TeamLeaderController : Controller
+    {
+
+        private readonly IUnitOfWork unitofwork;
+        public TeamLeaderController(IUnitOfWork unitofwork)
+        {
+            this.unitofwork = unitofwork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EngineersView(int teamid)
+        {
+            var engieers = unitofwork.Engineers.GetEngineers(teamid);
+            return View(engieers);
+        }
+
+        [HttpGet]
+        public IActionResult displaySubTasks(string engId)
+        {
+            var subtask = unitofwork.EngineerSubTasks.GetSubTasksFromEngineerId(engId).ToList();
+            return PartialView("_SubTaskPartialView", subtask);
+        }
+    }
+}
