db6ffb4f5a051addc6960fa0614b891988fa7a5e
MohamedShaker <M.elzantot@gmail.com>
2020-05-26 05:29:30 +0200

    Shaker

--- ITI.CEI40.Monitor/Controllers/SubTaskController.cs
+++ ITI.CEI40.Monitor/Controllers/SubTaskController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ITI.CEI40.Monitor.Data;
 using ITI.CEI40.Monitor.Entities;
+using ITI.CEI40.Monitor.Models.View_Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ITI.CEI40.Monitor.Controllers
