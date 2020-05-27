212ae9556cc8a3ebf4fbeeaccc1429d667a7d5c5
Omar <omarelsafty9@gmail.com>
2020-05-27 06:54:29 +0200

    try2.2

---
+++ ITI.CEI40.Monitor/Data/Migrations/20200527013334_edittaskprogress.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITI.CEI40.Monitor.Migrations
+{
+    public partial class edittaskprogress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Progress",
+                table: "Task",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Progress",
+                table: "Task",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
