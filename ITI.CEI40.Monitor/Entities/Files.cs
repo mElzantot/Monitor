8a5973344bc29c9979e98d587c288fc90c753678
MohamedShaker <M.elzantot@gmail.com>
2020-06-11 05:47:40 +0200

    FilesUpdate

--- ITI.CEI40.Monitor/Entities/Files.cs
+++ ITI.CEI40.Monitor/Entities/Files.cs
@@ -10,32 +10,38 @@ namespace ITI.CEI40.Monitor.Entities
 {
     public class Files
     {
+
         [Key]
         public int Id { get; set; }
-
-        [ForeignKey("Sender")]
-        public string FK_SenderId { get; set; }
-        public ApplicationUser Sender { get; set; }
-
-        public string RecieverId { get; set; }
-
-        public FileType FileType { get; set; }
-
-        public DateTime Time { get; set; }
+        //public FileType FileType { get; set; }
         public string FilePath { get; set; }
 
-        [ForeignKey("Project")]
-        public int? FK_ProjectId { get; set; }
-        public Project Project { get; set; }
+        [ForeignKey(nameof(Comment))]
+        public int CommentID { get; set; }
 
-        [ForeignKey("Task")]
-        public int? FK_TaskId { get; set; }
-        public Activity Task { get; set; }
+        public Comment Comment { get; set; }
+
+        //[ForeignKey("Sender")]
+        //public string FK_SenderId { get; set; }
+
+        //public ApplicationUser Sender { get; set; }
+
+        //public string RecieverId { get; set; }
+
+        //public DateTime Time { get; set; }
+
+        //[ForeignKey("Project")]
+        //public int? FK_ProjectId { get; set; }
+        //public Project Project { get; set; }
+
+        //[ForeignKey("Task")]
+        //public int? FK_TaskId { get; set; }
+        //public Activity Task { get; set; }
 
 
-        [ForeignKey("SubTask")]
-        public int? FK_SubTaskId { get; set; }
-        public SubTask SubTask { get; set; }
+        //[ForeignKey("SubTask")]
+        //public int? FK_SubTaskId { get; set; }
+        //public SubTask SubTask { get; set; }
 
     }
 }
