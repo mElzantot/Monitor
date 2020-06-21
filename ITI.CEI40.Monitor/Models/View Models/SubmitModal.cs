0b16225cf00bab5122a381c24729504620f781ab
Omar <omarelsafty9@gmail.com>
2020-06-22 00:46:12 +0200

    1

---
+++ ITI.CEI40.Monitor/Models/View Models/SubmitModal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.CEI40.Monitor.Models.View_Models
+{
+    public class SubmitModal
+    {
+        public int ID { get; set; }
+        public int Complexity { get; set; }
+        public int Quality { get; set; }
+        public int TimeManagement { get; set; }
+    }
+}
